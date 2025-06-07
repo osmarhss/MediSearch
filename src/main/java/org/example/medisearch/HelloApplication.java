caa5d2175a19a72b6118f7b7c56117575a469073
osmar <osmarhenriquecn@hotmail.com>
2025-06-07 17:35:49 -0300

    Criação das classes DAO e ajusdes nos modelos de domínio

--- src/main/java/org/example/medisearch/HelloApplication.java
+++ src/main/java/org/example/medisearch/HelloApplication.java
@@ -1,5 +1,6 @@
 package org.example.medisearch;
 
+import DbConnection.DatabaseManager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -20,6 +21,6 @@ public class HelloApplication extends Application {
     public static void main(String[] args) {
 
         launch();
-        SQLiteConnection.connect();
+        DatabaseManager.getConnection();
     }
 }
